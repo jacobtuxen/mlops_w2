ae6dfc92d034e3069cb4f75c5ffa27fc12d3d6df
jacobtuxen <jacob.tuxen@outlook.com>
2025-01-13 12:45:22 +0100

    :turtle: ignore some test

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,6 +1,9 @@
 from week2.train import train
+import os
+import pytest
+from tests import _PATH_DATA
 
-
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
 def test_train():
     train(lr=1e-3, epochs=1, output_path="models/model.pt", save_model=False)
     assert True
