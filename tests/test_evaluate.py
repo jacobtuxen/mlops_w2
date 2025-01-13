d81e1895e319680b137ff9f37de14193c0a078da
jacobtuxen <jacob.tuxen@outlook.com>
2025-01-13 12:42:55 +0100

    :turtle: ignore some test

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,9 +1,15 @@
+import os
+
+import pytest
 import torch
 from week2.data import CorruptMNIST
 from week2.evaluate import evaluate
 from week2.model import MyAwesomeModel
 
+from tests import _PATH_DATA
 
+
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
 def test_evalaute():
     dataset = CorruptMNIST("data/raw/corruptmnist_v1", split="test")
     dataset_dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False)
